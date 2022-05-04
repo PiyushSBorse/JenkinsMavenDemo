e63184935c77fd712e38bb9cf2e0b9c313551deb
Piyush Borse <piyush_borse@persistent.com>
2022-05-04 21:25:58 +0530

    Added Maven Material

---
+++ src/main/java/TimeDate.java
@@ -0,0 +1,17 @@
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
+
+public class TimeDate {
+	
+	public static void main(String args[])
+	{
+		SimpleDateFormat formatDate = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss z");
+		
+		Date date = new Date();
+
+		formatDate.setTimeZone(TimeZone.getTimeZone("IST"));
+
+		System.out.println(formatDate.format(date));
+	}
+}
